7c3a5a9ca21cf25f5307a6d3d854ab2074985249
pedmiston <pierce.edmiston@gmail.com>
2015-02-09 08:04:09 -0600

    Home page redirects to game list

--- telephone/telephone/urls.py
+++ telephone/telephone/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^grunt/', include('grunt.urls')),
+    url(r'^$', RedirectView.as_view(pattern_name = 'game_list')),
+    url(r'^games/', include('grunt.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, 
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root = settings.MEDIA_ROOT)
