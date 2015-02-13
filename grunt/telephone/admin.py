69dfa0b4888890d36b4c3e324abc7e70da269c8d
pedmiston <pierce.edmiston@gmail.com>
2015-02-13 11:35:32 -0600

    Removed registration of Entry to model admin

--- grunt/telephone/admin.py
+++ grunt/telephone/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Game, Seed, Cluster, Chain, Entry
+from .models import Game, Seed, Cluster, Chain
 
-@admin.register(Game, Seed, Cluster, Chain, Entry)
+@admin.register(Game, Seed, Cluster, Chain)
 class GameAdmin(admin.ModelAdmin):
     pass
