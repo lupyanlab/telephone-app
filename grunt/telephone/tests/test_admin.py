6f5921e78ab5a447aeec6c58872c08db5f34a42f
pedmiston <pierce.edmiston@gmail.com>
2015-02-13 11:09:02 -0600

    First admin model tests

---
+++ grunt/telephone/tests/test_admin.py
@@ -0,0 +1,17 @@
+
+from django.contrib.admin.sites import AdminSite
+from django.test import TestCase, override_settings
+
+from telephone.admin import GameAdmin
+from telephone.models import Game
+
+class ModelAdminTests(TestCase):
+    def setUp(self):
+        self.site = AdminSite()
+        self.request = None
+
+    def test_default_fields(self):
+       game_admin = GameAdmin(Game, self.site)
+       admin_form = game_admin.get_form(self.request)
+       admin_fields = list(admin_form.base_fields)
+       self.assertEquals(admin_fields, ['name', 'order', 'completion_code'])
