31d90cb3e6c2454d86e6b63aff30541f592c5413
pedmiston <pierce.edmiston@gmail.com>
2015-05-27 09:35:36 -0500

    Added new game element to games list items

--- grunt/ftests/test_make_game.py
+++ grunt/ftests/test_make_game.py
@@ -15,13 +15,28 @@ class MakeGameTest(FunctionalTest):
         """ Simulate a user making a new game """
         self.nav_to_games_list()
 
-        # Marcus sees a single button to create a new game.
+        # Marcus sees a single button to create a new game
         games_list = self.browser.find_element_by_id('id_game_list')
         games = games_list.find_elements_by_tag_name('li')
         self.assertEqual(len(games), 1)
         new_game_item = games[0]
 
-        self.fail('Finish the test!')
+        # He clicks the button to create a new game
+        new_game_item.find_element_by_tag_name('a').click()
+
+        # Marcus fills out the new game form
+        new_game_name = 'My New Game'
+        self.browser.find_element_by_id('id_name').send_keys(new_game_name)
+        self.browser.find_element_by_id('id_submit').click()
+
+        # He sees his new game on the game list page
+        games_list = self.browser.find_element_by_id('id_game_list')
+        games = games_list.find_elements_by_tag_name('li')
+        self.assertEquals(len(games), 2)
+        my_new_game = games[1]
+        my_new_game_name = my_new_game.find_element_by_tag('h2').text
+        self.assertEquals(my_new_game_name, new_game_name,
+                          'Name on form not in games list')
 
     def test_upload_seed(self):
         game_name = 'Empty Game'
