364774d482f3b8b91e1575c856170e6f5345b04e
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-06-19 16:27:19 -0500

    Production settings

--- telephone-app/settings/prod.py
+++ telephone-app/settings/prod.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['grunt.pedmiston.xyz', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'telephone',
+        'USER': 'telephone',
+        'PASSWORD': 'telephonepass',
+        'HOST': environ.get('POSTGRESQL_HOST', 'localhost'),
+        'PORT': '',
+    },
+}
