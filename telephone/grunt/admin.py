03318948f9440bdb3b25a02dda03191e451710e9
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-08-24 12:34:05 -0500

    Removed submodule

--- telephone/grunt/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Game, Chain, Message
-
-@admin.register(Game, Chain, Message)
-class GameAdmin(admin.ModelAdmin):
-    pass
--- telephone/grunt/handlers.py
+++
@@ -1,8 +0,0 @@
-from unipath import Path
-
-def message_path(instance, filename):
-    """ Determine the filename and path for a new audio message """
-    message_dir = instance.chain.dirpath()
-    stem_kwargs = {'generation': instance.generation}
-    message_stem = '{generation}.wav'.format(**stem_kwargs)
-    return Path(message_dir, message_stem)
