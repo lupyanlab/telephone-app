a686c2668f726d0ff1fe7e29a622ed04546f2a5e
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-03-06 22:12:58 -0600

    Pipeline for getting data off of server

--- tasks.py
+++ tasks.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 from invoke import run, task
 import boto3
+from unipath import Path
 
 bucket_name = 'telephone-app'
 
 @task
-def download_experiment(output_dir):
-    run('ansible-playbook snapshot.yml')
+def download_experiment(snapshot_name='words-in-transition'):
+    """Take a snapshot of the data on the server and store it on S3."""
+    cmd = 'ansible-playbook snapshot.yml -e snapshot_name={}'
+    run(cmd.format(snapshot_name))
+
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
     # download db and media files
-    
+    for key in Path(snapshot_name).listdir():
+        with open(key, 'rb') as f:
+            bucket.put_object(Body=f, Key=key)
